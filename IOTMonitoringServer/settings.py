b8d592e42ca6fda371a93a6eb4691dc48d038563
Jose Manuel Vega Alarcon <jm.vegaa1@uniandes.edu.co>
2023-09-26 11:10:40 -0500

    update

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "54.147.161.15"]
+ALLOWED_HOSTS = ["localhost", "3.81.226.175"]
 
 
 # Application definition
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "54.147.138.209",  # Dirección IP de la base de datos
+        "HOST": "54.89.104.95",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -156,7 +156,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "54.172.150.221"
+MQTT_HOST = "54.209.203.66"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
